eaa9a2bb4eb4ba3d8a14f46e23322ba165920633
pixelstudiosmc <jacobhmollan@gmail.com>
2022-04-13 15:03:02 +0100

    enter

---
+++ scripts/sound.js
@@ -0,0 +1,6 @@
+var book = document.getElementById("book");
+var strt = document.getElementById("strt")
+
+var booksound = new Audio("assets/book.mp3")
+booksound.volume = 0.5;
+var glowsound = new Audio("assets/glow.wav")
